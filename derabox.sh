328790001e5a91e7fef1a83791983db0edd7d3ec
Assnsster <anous123bg@gmail.com>
2024-08-25 21:34:53 +0700

    Create derabox.sh

---
+++ derabox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Color
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+light_cyan='\033[1;96m'
+reset='\033[0m'
+#/
+# Check Architecture
+echo -ne "$yellow Kiểm tra Số thứ..."
+case $(getprop ro.product.cpu.abi) in
+        arm64-v8a)echo -ne "$yellow Architecture của bạn là $green $(getprop ro.product.cpu.abi)"
+        *)echo -ne "$yellow Architecture của bạn là $red $(getprop ro.product.cpu.abi)" ; echo -ne "$red Architecture của bạn không hỗ trợ!"
+esac
