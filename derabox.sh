6f350a103ab58f308ed9c1434f0dd338e7f730c0
Assnsster <anous123bg@gmail.com>
2024-08-26 07:28:49 +0700

    Update derabox.sh

--- derabox.sh
+++ derabox.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Color
 red='\033[1;31m'
 green='\033[1;32m'
@@ -7,13 +8,14 @@ blue='\033[1;34m'
 light_cyan='\033[1;96m'
 r='\033[0m'
 #/
+clear
 # Check Architecture
-echo -ne "$yellow Kiểm tra Số thứ..."
+echo -ne "$yellow Kiểm tra Số thứ...\n"
 case $(getprop ro.product.cpu.abi) in
-        arm64-v8a)echo -ne "$yellow Architecture của bạn là $green $(getprop ro.product.cpu.abi)";;
-        *)echo -ne "$yellow Architecture của bạn là $red $(getprop ro.product.cpu.abi)" ; echo -ne "$red Architecture của bạn không hỗ trợ! $r";;
+        arm64-v8a)echo -ne "$yellow Architecture của bạn là $green $(getprop ro.product.cpu.abi)"\n;;
+        *)echo -ne "$yellow Architecture của bạn là $red $(getprop ro.product.cpu.abi)\n" ; echo -ne "$red Architecture của bạn không hỗ trợ! $r\n";exit;;
 esac
 # check dir
 if [ ! -d $PREFIX/opt/derabox ]
-  then echo -ne "$yellow derabox is"$red" not install!"
+  then echo -ne "$yellow derabox is"$red" not install!\n"
 fi
